5fcf945aa9863293d014e3afa0e406e7f74d3da4
donfyla <alabioyetope@gmail.com>
2026-03-15 12:58:31 +0100

    feat: Add migration to update Oluwadurotimi's rank to Nigeria's #12

---
+++ supabase/migrations/050_update_oluwadurotimi_rank.sql
@@ -0,0 +1,11 @@
+-- Update Oluwadurotimi's rank_title to Nigeria's #12
+-- This will show the badge on his elite coach card
+
+UPDATE coaches
+SET rank_title = 'Nigeria\'s #12'
+WHERE name ILIKE '%Oluwadurotimi%';
+
+-- Verify the update
+SELECT id, name, rank_title, is_special
+FROM coaches
+WHERE name ILIKE '%Oluwadurotimi%';
